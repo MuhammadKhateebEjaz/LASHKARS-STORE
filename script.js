913dc252c3a366261ff82bc5e4f04165d8481f2f
Muhammad Khateeb Ejaz <160773008+MuhammadKhateebEjaz@users.noreply.github.com>
2026-06-24 23:46:56 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,145 +1,100 @@
-
-// Get Elements
+// ===== ORDER MODAL =====
 
 const modal = document.getElementById("orderModal");
-
 const productName = document.getElementById("productName");
-
 const productInput = document.getElementById("productInput");
-
 const priceInput = document.getElementById("priceInput");
 
-
-
-
-// OPEN ORDER POPUP
+// Open Order Popup
 
 function openOrder(product, price){
 
-
     modal.style.display = "flex";
 
-
     productName.innerHTML =
-    "Ordering: " + product + 
+    "Ordering: " + product +
     "<br>Price: Rs. " + price;
 
-
-
     productInput.value = product;
-
-
     priceInput.value = "Rs. " + price;
 
-
-
-    // Disable background scroll
-
-    document.body.style.overflow="hidden";
-
-
+    document.body.style.overflow = "hidden";
 }
 
-
-
-
-
-
-// CLOSE ORDER POPUP
-
+// Close Order Popup
 
 function closeOrder(){
 
+    modal.style.display = "none";
 
-    modal.style.display="none";
-
-
-    document.body.style.overflow="auto";
-
-
+    document.body.style.overflow = "auto";
 }
 
+// ===== CART SYSTEM =====
 
+let cart = [];
 
+// Add Product To Cart
 
+function addCart(name, price){
 
+    cart.push({
+        name: name,
+        price: price
+    });
 
-// Close when click outside
+    document.getElementById("cartCount").innerHTML = cart.length;
 
-
-window.onclick=function(event){
-
-
-    if(event.target == modal){
-
-
-        closeOrder();
-
-
-    }
-
-
+    alert(name + " Added To Cart 🛒");
 }
 
-
-
-
-let cart=[];
-
-
-function addCart(name,price){
-
-
-cart.push({
-
-name:name,
-price:price
-
-});
-
-
-document.getElementById("cartCount").innerHTML = cart.length;
-
-
-alert(name+" Added To Cart 🛒");
-
-
-}
-
-
+// Open Cart
 
 function openCart(){
 
+    let box = document.getElementById("cartItems");
 
-let box=document.getElementById("cartItems");
+    if(cart.length === 0){
 
+        box.innerHTML = "<p>Your cart is empty</p>";
 
-box.innerHTML="";
+    }else{
 
+        box.innerHTML = "";
 
-cart.forEach(item=>{
+        cart.forEach(item => {
 
+            box.innerHTML += `
+            <p>
+                ${item.name} - Rs ${item.price}
+            </p>
+            `;
 
-box.innerHTML += `
+        });
 
-<p>
-${item.name} - Rs ${item.price}
-</p>
+    }
 
-`;
-
-});
-
-
-document.getElementById("cartModal").style.display="flex";
-
-
+    document.getElementById("cartModal").style.display = "flex";
 }
 
-
+// Close Cart
 
 function closeCart(){
 
-document.getElementById("cartModal").style.display="none";
+    document.getElementById("cartModal").style.display = "none";
+}
 
+// Close Modal When Click Outside
+
+window.onclick = function(event){
+
+    if(event.target == modal){
+
+        closeOrder();
+    }
+
+    if(event.target == document.getElementById("cartModal")){
+
+        closeCart();
+    }
 }
